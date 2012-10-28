4b627a19a37802589588406123b95e8eee13f844
nDy <andresrangel.1991@gmail.com>
2012-10-27 22:02:16 -0430

    Character class

---
+++ DnD/lib/bodies/Character.h
@@ -0,0 +1,41 @@
+/*
+ * Character.h
+ *
+ *  Created on: Oct 27, 2012
+ *      Author: ndy
+ */
+
+#ifndef CHARACTER_H_
+#define CHARACTER_H_
+
+#include "Body.h"
+#include "../grid/SquareGrid.H"
+
+class Character: public Body {
+private:
+	int life;
+	int speed;
+	SquareGrid* grid;
+
+public:
+	Character(int X, int Y, SquareGrid* Grid) :
+			Body(X, Y) {
+		this->grid = Grid;
+		this->speed = 0;
+	}
+
+	void setSpeed(int Speed) {
+		this->speed = Speed;
+	}
+
+	bool moveTo(int X, int Y) {
+		if (!grid->switchPointers(this->getPosX(), this->getPosY(), X, Y))
+			return false;
+		this->setPosX(X);
+		this->setPosY(Y);
+		return true;
+	}
+
+};
+
+#endif /* CHARACTER_H_ */
