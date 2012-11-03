cabea35be525f40afb20db160c244c83fd5d3e38
nDy <andresrangel.1991@gmail.com>
2012-11-03 14:11:59 -0430

    Lista y nodo incluidos el grid necesita redise;arse para grafos

--- DnD/lib/bodies/Character.h
+++ DnD/lib/bodies/Character.h
@@ -10,8 +10,8 @@
 
 #include "Body.h"
 #include "../grid/SquareGrid.H"
-#include "cmath"
-
+#include <cmath>
+#include <iostream>
 class Character: public Body {
 private:
 	int life;
@@ -39,152 +39,160 @@ public:
 
 	void __getAstarPath(int**path, int X, int Y, int goalX, int goalY,
 			int size) {
+		std::cout << "Lanza la funcion privada" << std::endl;
 		int listSize;
 		int** priorityPositionList;
 		double* priorityList;
 		int pos = 0;
 
-		if (x > 0 && y > 0 && x < SquareGrid::STDWIDTH
-				&& y < SquareGrid::STDHEIGHT) {
-			listSize = 8;
-		} else {
-			listSize = 6;
-		}
-
+		/*if (X > 0 && Y > 0 && X < SquareGrid::STDWIDTH
+		 && Y < SquareGrid::STDHEIGHT) {*/
+		listSize = 8;
+		/*} else {
+		 listSize = 6;
+		 }*/
+		std::cout << "Llega a la instanciacion de los vectores de prioridad"
+				<< std::endl;
 		priorityList = new double[listSize];
-		priorityPositionList = new int[listSize][2];
+		std::cout << "crea el vector de prioridad" << std::endl;
+		priorityPositionList = new int*[listSize];
+		std::cout << "crea el vector de pares prioridad" << std::endl;
+		for (int var = 0; var < listSize; ++var) {
+			priorityPositionList[var] = new int[2];
+			std::cout << "Inicializa vector de par de prioridad" << std::endl;
+		}
+		std::cout << "crea los vectores de prioridad" << std::endl;
 
-		if (x > 0 && y > 0 && this->grid->getBody(x - 1, y - 1) == NULL) {
-			if (!this->grid->getTerrain(x - 1, y - 1)->wasVisited()) {
-				this->grid->getTerrain(x - 1, y - 1)->visit();
-				priorityPositionList[pos][0] = x - 1;
-				priorityPositionList[pos][1] = y - 1;
+		if (X > 0 && Y > 0 && this->grid->getBody(X - 1, Y - 1) == NULL) {
+			if (!this->grid->getTerrain(X - 1, Y - 1)->wasVisited()) {
+				this->grid->getTerrain(X - 1, Y - 1)->visit();
+				priorityPositionList[pos][0] = X - 1;
+				priorityPositionList[pos][1] = X - 1;
 				priorityList[pos] = sqrt(
-						pow(goalX - x - 1, 2) + pow(goalY - y - 1, 2));
+						pow(goalX - X - 1, 2) + pow(goalY - X - 1, 2));
 				pos++;
 			}
 		}
 
-		if (y > 0 && this->grid->getBody(x, y - 1) == NULL) {
-			if (!this->grid->getTerrain(x, y - 1)->wasVisited()) {
-				this->grid->getTerrain(x, y - 1)->visit();
-				priorityPositionList[pos][0] = x;
-				priorityPositionList[pos][1] = y - 1;
+		if (Y > 0 && this->grid->getBody(X, Y - 1) == NULL) {
+			if (!this->grid->getTerrain(X, Y - 1)->wasVisited()) {
+				this->grid->getTerrain(X, Y - 1)->visit();
+				priorityPositionList[pos][0] = X;
+				priorityPositionList[pos][1] = Y - 1;
 				priorityList[pos] = sqrt(
-						pow(goalX - x, 2) + pow(goalY - y - 1, 2));
+						pow(goalX - X, 2) + pow(goalY - Y - 1, 2));
 				pos++;
 			}
 		}
 
-		if (x < SquareGrid::STDWIDTH && y > 0
-				&& this->grid->getBody(x + 1, y - 1) == NULL) {
-			if (x > 0 && y > 0 && this->grid->getBody(x + 1, y - 1) == NULL) {
-				if (!this->grid->getTerrain(x + 1, y - 1)->wasVisited()) {
-					this->grid->getTerrain(x + 1, y - 1)->visit();
-					priorityPositionList[pos][0] = x + 1;
-					priorityPositionList[pos][1] = y - 1;
+		if (X < SquareGrid::STDWIDTH&& Y > 0
+		&& this->grid->getBody(X + 1, Y - 1) == NULL) {if(X > 0 && Y > 0 && this->grid->getBody(X + 1, Y - 1) == NULL) {
+			if (!this->grid->getTerrain(X + 1, Y - 1)->wasVisited()) {
+				this->grid->getTerrain(X + 1, Y - 1)->visit();
+				priorityPositionList[pos][0] = X + 1;
+				priorityPositionList[pos][1] = Y - 1;
+				priorityList[pos] = sqrt(
+				pow(goalX - X + 1, 2) + pow(goalY - Y - 1, 2));
+				pos++;
+			}
+		}
+
+		if (X < SquareGrid::STDWIDTH && this->grid->getBody(X + 1, Y) == NULL) {
+			if (X > 0 && Y > 0 && this->grid->getBody(X + 1, Y) == NULL) {
+				if (!this->grid->getTerrain(X + 1, Y)->wasVisited()) {
+					this->grid->getTerrain(X + 1, Y)->visit();
+					priorityPositionList[pos][0] = X + 1;
+					priorityPositionList[pos][1] = Y;
 					priorityList[pos] = sqrt(
-							pow(goalX - x + 1, 2) + pow(goalY - y - 1, 2));
+					pow(goalX - X + 1, 2) + pow(goalY - Y, 2));
 					pos++;
 				}
 			}
 		}
 
-		if (x < SquareGrid::STDWIDTH && this->grid->getBody(x + 1, y) == NULL) {
-			if (x > 0 && y > 0 && this->grid->getBody(x + 1, y) == NULL) {
-				if (!this->grid->getTerrain(x + 1, y)->wasVisited()) {
-					this->grid->getTerrain(x + 1, y)->visit();
-					priorityPositionList[pos][0] = x + 1;
-					priorityPositionList[pos][1] = y;
+		if (X < SquareGrid::STDWIDTH&& Y < SquareGrid::STDHEIGHT
+		&& this->grid->getBody(X + 1, Y + 1) == NULL) {if (X > 0 && Y > 0 && this->grid->getBody(X + 1, Y + 1) == NULL) {
+				if (!this->grid->getTerrain(X + 1, Y + 1)->wasVisited()) {
+					this->grid->getTerrain(X + 1, Y + 1)->visit();
+					priorityPositionList[pos][0] = X + 1;
+					priorityPositionList[pos][1] = Y + 1;
 					priorityList[pos] = sqrt(
-							pow(goalX - x + 1, 2) + pow(goalY - y, 2));
+					pow(goalX - X + 1, 2) + pow(goalY - Y + 1, 2));
 					pos++;
 				}
 			}
 		}
 
-		if (x < SquareGrid::STDWIDTH && y < SquareGrid::STDHEIGHT
-				&& this->grid->getBody(x + 1, y + 1) == NULL) {
-			if (x > 0 && y > 0 && this->grid->getBody(x + 1, y + 1) == NULL) {
-				if (!this->grid->getTerrain(x + 1, y + 1)->wasVisited()) {
-					this->grid->getTerrain(x + 1, y + 1)->visit();
-					priorityPositionList[pos][0] = x + 1;
-					priorityPositionList[pos][1] = y + 1;
+		if (Y < SquareGrid::STDHEIGHT && this->grid->getBody(X, Y + 1) == NULL) {
+			if (X > 0 && Y > 0 && this->grid->getBody(X, Y + 1) == NULL) {
+				if (!this->grid->getTerrain(X, Y + 1)->wasVisited()) {
+					this->grid->getTerrain(X, Y + 1)->visit();
+					priorityPositionList[pos][0] = X;
+					priorityPositionList[pos][1] = Y + 1;
 					priorityList[pos] = sqrt(
-							pow(goalX - x + 1, 2) + pow(goalY - y + 1, 2));
+					pow(goalX - X, 2) + pow(goalY - Y + 1, 2));
 					pos++;
 				}
 			}
 		}
 
-		if (y < SquareGrid::STDHEIGHT
-				&& this->grid->getBody(x, y + 1) == NULL) {
-			if (x > 0 && y > 0 && this->grid->getBody(x, y + 1) == NULL) {
-				if (!this->grid->getTerrain(x, y + 1)->wasVisited()) {
-					this->grid->getTerrain(x, y + 1)->visit();
-					priorityPositionList[pos][0] = x;
-					priorityPositionList[pos][1] = y + 1;
+		if (X > 0&& Y < SquareGrid::STDHEIGHT
+		&& this->grid->getBody(X - 1, Y + 1) == NULL) {if (X > 0 && Y > 0 && this->grid->getBody(X - 1, Y + 1) == NULL) {
+				if (!this->grid->getTerrain(X - 1, Y + 1)->wasVisited()) {
+					this->grid->getTerrain(X - 1, Y + 1)->visit();
+					priorityPositionList[pos][0] = X - 1;
+					priorityPositionList[pos][1] = Y + 1;
 					priorityList[pos] = sqrt(
-							pow(goalX - x, 2) + pow(goalY - y + 1, 2));
+					pow(goalX - X - 1, 2) + pow(goalY - Y + 1, 2));
 					pos++;
 				}
 			}
 		}
 
-		if (x > 0 && y < SquareGrid::STDHEIGHT
-				&& this->grid->getBody(x - 1, y + 1) == NULL) {
-			if (x > 0 && y > 0 && this->grid->getBody(x - 1, y + 1) == NULL) {
-				if (!this->grid->getTerrain(x - 1, y + 1)->wasVisited()) {
-					this->grid->getTerrain(x - 1, y + 1)->visit();
-					priorityPositionList[pos][0] = x - 1;
-					priorityPositionList[pos][1] = y + 1;
+		if (X > 0 && this->grid->getBody(X - 1, Y) == NULL) {
+			if (X > 0 && Y > 0 && this->grid->getBody(X - 1, Y) == NULL) {
+				if (!this->grid->getTerrain(X - 1, Y)->wasVisited()) {
+					this->grid->getTerrain(X - 1, Y)->visit();
+					priorityPositionList[pos][0] = X - 1;
+					priorityPositionList[pos][1] = Y;
 					priorityList[pos] = sqrt(
-							pow(goalX - x - 1, 2) + pow(goalY - y + 1, 2));
+					pow(goalX - X - 1, 2) + pow(goalY - Y, 2));
 					pos++;
 				}
 			}
 		}
-
-		if (x > 0 && this->grid->getBody(x - 1, y) == NULL) {
-			if (x > 0 && y > 0 && this->grid->getBody(x - 1, y) == NULL) {
-				if (!this->grid->getTerrain(x - 1, y)->wasVisited()) {
-					this->grid->getTerrain(x - 1, y)->visit();
-					priorityPositionList[pos][0] = x - 1;
-					priorityPositionList[pos][1] = y;
-					priorityList[pos] = sqrt(
-							pow(goalX - x - 1, 2) + pow(goalY - y, 2));
-					pos++;
-				}
-			}
-		}
+		std::cout << "revisa los espacios adyacentes" << std::endl;
 		//ordenar
 		for (int i = 0; i < listSize; ++i) {
 			for (int j = i + 1; j < listSize; ++j) {
 				if (priorityList[i] > priorityList[j])
-					for (int k = j; k > 0; --k) {
-						double temp = priorityList[k];
-						int tempx = priorityPositionList[k][0];
-						int tempy = priorityPositionList[k][1];
-						priorityList[k] = priorityList[k - 1];
-						priorityPositionList[k][0] =
-								priorityPositionList[k - 1][0];
-						priorityPositionList[k][1] =
-								priorityPositionList[k - 1][1];
-						priorityList[k - 1] = temp;
-						priorityPositionList[k - 1][0] = tempx;
-						priorityPositionList[k - 1][1] = tempy;
-					}
+				for (int k = j; k > 0; --k) {
+					double temp = priorityList[k];
+					int tempx = priorityPositionList[k][0];
+					int tempy = priorityPositionList[k][1];
+					priorityList[k] = priorityList[k - 1];
+					priorityPositionList[k][0] =
+					priorityPositionList[k - 1][0];
+					priorityPositionList[k][1] =
+					priorityPositionList[k - 1][1];
+					priorityList[k - 1] = temp;
+					priorityPositionList[k - 1][0] = tempx;
+					priorityPositionList[k - 1][1] = tempy;
+				}
 			}
 		}
 
 		if (priorityList[0] == 0) {
-			path = new int[size][2];
+			path = new int*[size];
+			for (int var = 0; var < size; ++var) {
+				path[var] = new int[2];
+			}
 			path[size - 1][0] = priorityPositionList[0][0];
 			path[size - 1][1] = priorityPositionList[0][1];
 		} else {
 			for (int var = 0; var < pos; ++var) {
 				__getAstarPath(path, priorityPositionList[var][0],
-						priorityPositionList[var][1], goalX, goalY, size + 1);
+				priorityPositionList[var][1], goalX, goalY, size + 1);
 				if (path != NULL) {
 					path[size - 1][0] = priorityPositionList[var][0];
 					path[size - 1][1] = priorityPositionList[var][1];
@@ -193,13 +201,16 @@ public:
 			}
 		}
 	}
-
-	int** getAstarPath(int X, int Y) {
-		int** path;
-		__getAstarPath(path, this->getPosX(), this->getPosY(), X, Y, 0);
-		return path;
-	}
-
-};
+}
+int** getAstarPath(int X, int Y) {
+	std::cout << "lanza la interfaz" << std::endl;
+	int** path;
+	path = NULL;
+	std::cout << "se trata de lanzar la funcion privada" << std::endl;
+	__getAstarPath(path, this->getPosX(), this->getPosY(), X, Y, 0);
+	return path;
+}
+}
+;
 
 #endif /* CHARACTER_H_ */
--- DnD/src/main.cpp
+++ DnD/src/main.cpp
@@ -8,34 +8,10 @@
 #include "../lib/game/DnD.h"
 #include "../lib/bodies/Character.h"
 #include "../lib/grid/SquareGrid.H"
+#include "../lib/common/List/SList.H"
 #include <iostream>
 
 int main(int argc, char **argv) {
-
-	SquareGrid* grid;
-	std::cout << "Se crea el puntero " << std::endl;
-	grid = new SquareGrid();
-	std::cout << "Se instancia el grid " << std::endl;
-
-	grid->getBody(19, 8) = new Character(19, 8, grid);
-
-	grid->getBody(19, 9) = new Character(19, 9, grid);
-
-	((Character*) grid->getBody(19, 8))->moveTo(19, 7);
-
-	for (int var = 0; var < 20; ++var) {
-		for (int vartwo = 0; vartwo < 10; ++vartwo) {
-			std::cout << "Cuerpo que se supone debe estar en " << var << " "
-					<< vartwo << std::endl;
-			if (grid->getBody(var, vartwo) == NULL) {
-				std::cout << "No tiene un cuerpo sobre ella" << std::endl;
-			} else
-				std::cout << "Se encuentra en "
-						<< grid->getBody(var, vartwo)->getPosX() << " "
-						<< grid->getBody(var, vartwo)->getPosY() << std::endl;
-		}
-	}
-
 	/*
 	 DnD* Game;
 
